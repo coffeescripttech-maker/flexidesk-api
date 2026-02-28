2d3df48a6bcee53ee0a8976a3a2a42d15423dc3e
xedter20 <dextermiranda441@gmail.com>
2026-03-01 06:06:55 +0800

    fix expirty

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function signJwt(payload, opts = {}) {
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "7d", ...opts });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "120d", ...opts });
 }
 function verifyJwt(token) {
   return jwt.verify(token, process.env.JWT_SECRET);
